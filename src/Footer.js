caeb0dbb0496e6fa07e158ca4c8493dfbff1bb9b
D-souz <77883365+D-souz@users.noreply.github.com>
2023-05-25 20:56:58 +0300

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div>
-             <p className="footer">Click<a href="#"> here </a> to view source code</p>
+             <p className="footer">Click<a href="https://github.com/D-souz/Travel-journal"> here </a> to view source code</p>
         </div>
     );
 }
